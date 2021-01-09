7f1524eed593014ccf1add3f3d33cefe0774582a
Darius Task <darius@taskius.com>
2021-01-09 12:11:25 +0000

    Update to be birthday centric and set countdown date

--- js/birthday.js
+++ js/birthday.js
@@ -125,7 +125,7 @@ var interval = 10000;
 
 $(document).ready(function() {
 	var currentDate = new Date();
-	var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+	var futureDate  = new Date(2021, 0, 21);
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
 	clock = $clock.FlipClock(diff, {
